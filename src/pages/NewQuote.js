85f0122b2948db74e07b6e99c954ad219c7fa8e0
Farhan Sayed <farhanthegamer162@gmail.com>
2021-12-11 15:21:45 +0400

    Added Programmatic Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 
 const NewQuote = () => {
 
+    const history = useHistory();
     const addQuoteHandler = QuoteData => {
         console.log(QuoteData);
+
+        history.push('/quotes');
     };
 
     return <QuoteForm onAddQuote={addQuoteHandler}/>
